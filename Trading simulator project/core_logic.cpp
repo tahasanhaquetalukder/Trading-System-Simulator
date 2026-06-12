dd29d54b111e5f7290d9311147476f704e15beb1
tahasanhaquetalukder <tahasan5800p@gmail.com>
2026-06-12 19:55:20 +0600

    checking

--- Trading simulator project/core_logic.cpp
+++ Trading simulator project/core_logic.cpp
@@ -4,16 +4,117 @@
 using namespace std;
 bool User::Buy_stock(string stock_name, int quantity, double price)
 {
-	double total_cost = quantity * price;
-	if (total_cost > balance)
-	{
-		return false;
-	 }
-	for (size_t i = 0;i < portfolio.size();i++)
-	{
+    double total_cost = quantity * price;
+
+    if (total_cost > balance) {
+        return false;
+    }
+    for (size_t i = 0; i < portfolio.size(); i++) {
+        if (portfolio[i].stock_name == stock_name) {
+            double current_total_value = portfolio[i].quantity * portfolio[i].avarage_buy_price;
+            double new_total_value = current_total_value + total_cost;
+            int total_quantity = portfolio[i].quantity + quantity;
+
+            portfolio[i].avarage_buy_price = new_total_value / total_quantity;
+            portfolio[i].quantity += quantity;
+            balance -= total_cost;
+            Transaction new_transaction;
+            new_transaction.type = "Buy";
+            new_transaction.stock_name = stock_name;
+			new_transaction.quantity = quantity;
+			new_transaction.price = price;
+			transaction_history.push_back(new_transaction);
+
+
+            return true;
+        }
+    }
+    Portfolio_Item new_item;
+    new_item.stock_name = stock_name;
+    new_item.quantity = quantity;
+    new_item.avarage_buy_price = price;
+    portfolio.push_back(new_item);
+    balance -= total_cost;
+
+    Transaction new_transaction;
+    new_transaction.type = "Buy";
+    new_transaction.stock_name = stock_name;
+    new_transaction.quantity = quantity;
+    new_transaction.price = price;
+    transaction_history.push_back(new_transaction);
+    return true;
+}
+bool User::Sell_stock(string stock_name, int quantity, double price)
+{
+    for (size_t i = 0; i < portfolio.size(); i++)
+    {
+        
+        
 		if (portfolio[i].stock_name == stock_name)
-		{
-			double 
-		}
-	}
+        {
+            if (portfolio[i].quantity < quantity)
+            {
+                return false;
+            }
+            double revenue = quantity * price;
+            balance = balance + revenue;
+            portfolio[i].quantity -= quantity;
+            if (portfolio[i].quantity == 0)
+            {
+                portfolio.erase(portfolio.begin() + i);
+            }
+            Transaction new_transaction;
+            new_transaction.type = "Sell";
+            new_transaction.stock_name = stock_name;
+            new_transaction.quantity = quantity;
+            new_transaction.price = price;
+            transaction_history.push_back(new_transaction);
+
+			return true;
+        }
+    }
+    return false;
+}
+User::User(string username, string password)
+{
+    this->username = username;
+    this->password = password;
+	balance = 10000.0;
+}
+double User::getbalance()
+{
+    return balance;
+}
+vector<Portfolio_Item> User :: getportfolio()
+{
+	return portfolio;
+}
+bool User::add_money(double ammount)
+{
+    if (ammount < 0)
+    {
+        return false;
+    }
+    else
+    {
+		balance += ammount;
+		return true;
+    }
+}
+double User::check_profit_loss(string stock_name, double current_market_price)
+{
+    for (size_t i = 0;i < portfolio.size();i++)
+    {
+        if (portfolio[i].stock_name == stock_name)
+        {
+            double profit_loss_count = (current_market_price - portfolio[i].avarage_buy_price) * portfolio[i].quantity;
+            return profit_loss_count;
+        }
+        
+    }
+    return 0.0;
+}
+vector<Transaction> User::get_transaction_history()
+{
+    return transaction_history;
 }
--- Trading simulator project/core_logic.h
+++ Trading simulator project/core_logic.h
@@ -2,12 +2,11 @@
 #include<string>
 #include<vector>
 using namespace std;
-class Stock 
+class Stock
 {
 public:
 	string name;
 	double current_price;
-
 };
 class Portfolio_Item
 {
@@ -16,6 +15,13 @@ public:
 	int quantity;
 	double avarage_buy_price;
 };
+struct Transaction
+{
+	string type;
+	string stock_name;
+	int quantity;
+	double price;
+};
 class User
 {
 public:
@@ -23,14 +29,14 @@ public:
 	string password;
 	double balance;
 	vector<Portfolio_Item> portfolio;
-	User(string  username, string password)
-	{
-		this->username = username;
-		this->password = password;
-	}
-	User(){}
+	vector<Transaction> transaction_history;
+	User(string username, string password);
+	User();
 	bool Buy_stock(string stock_name, int quantity, double price);
 	bool Sell_stock(string stock_name, int quantity, double price);
-
-
+	double getbalance();
+	vector<Portfolio_Item> getportfolio();
+	bool add_money(double ammount);
+	double check_profit_loss(string stock_name, double current_market_price);
+	vector<Transaction> get_transaction_history();
 };
